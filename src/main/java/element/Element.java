c9fb3ed4d9a68671f9f9f9b8c5e8be670271bde4
limelier <tudor.iacobescu.1@gmail.com>
2020-03-13 15:40:21 +0200

    Added abstract Element class.

---
+++ src/main/java/element/Element.java
@@ -0,0 +1,22 @@
+package element;
+
+public abstract class Element {
+    private String name;
+
+    public Element(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
