72fbbbe5e9ec2c5315da32648c553a216a590ad4
limelier <tudor.iacobescu.1@gmail.com>
2020-03-13 15:51:08 +0200

    Removed Element.getName, added preferences.

--- src/main/java/element/Element.java
+++ src/main/java/element/Element.java
@@ -1,18 +1,28 @@
 package element;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class Element {
     String name;
+    List<Element> preferences;
 
     public Element(String name) {
         this.name = name;
+        preferences = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * Set the elements that this element accepts a matching with, in decreasing order of preference.
+     * @param preferences the preferences, in decreasing order
+     */
+    public void setPreferences(Element... preferences) {
+        this.preferences = Arrays.asList(preferences);
     }
 
     @Override
