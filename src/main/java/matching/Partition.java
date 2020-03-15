bf0524138247b3958fe55f45c99f6fc13157c23a
limelier <tudor.iacobescu.1@gmail.com>
2020-03-15 18:46:42 +0200

    Added the start of a Partition class.

---
+++ src/main/java/matching/Partition.java
@@ -0,0 +1,13 @@
+package matching;
+
+import element.Element;
+
+import java.util.List;
+
+public class Partition {
+    List<Element> elements;
+
+    public Partition(List<Element> elements) {
+        this.elements = elements;
+    }
+}
