bf8b9a938bd5ad6f1415bebbb6a0f299f8385d4a
limelier <tudor.iacobescu.1@gmail.com>
2020-03-16 01:59:43 +0200

    Added test file.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,25 @@
 package main;
 
+import algorithm.GaleShapleySolver;
+import algorithm.Solver;
+import element.Element;
+import element.Hospital;
+import element.Resident;
+import matchingProblem.Generator;
+import matchingProblem.Matching;
+import matchingProblem.Partition;
+import matchingProblem.Problem;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Generator g = new Generator();
+        Problem p = g.generate(5);
+        System.out.println(p);
+        Solver s = new GaleShapleySolver(p);
+        Matching m = s.solve();
+        System.out.println(m);
+        System.out.println(m.isStable());
     }
 }
