0a5f9cfe461746bfe19e891c28a79a5922421d58
limelier <tudor.iacobescu.1@gmail.com>
2020-03-16 02:00:40 +0200

    Remove unused Hospital and Resident.

--- src/main/java/element/Hospital.java
+++
@@ -1,7 +0,0 @@
-package element;
-
-public class Hospital extends Element {
-    public Hospital(String name) {
-        super(name);
-    }
-}
--- src/main/java/element/Resident.java
+++
@@ -1,7 +0,0 @@
-package element;
-
-public class Resident extends Element {
-    public Resident(String name) {
-        super(name);
-    }
-}
